2d73c51afd775de09a64ae32fec8f4f132b54dfd
Fabian <hifabian@ethz.ch>
2019-04-12 17:28:31 +0200

    Completed first version of HR-STM workchain

--- hrstm/hrstm_workchain.py
+++ hrstm/hrstm_workchain.py
@@ -9,10 +9,9 @@ from aiida.orm.code import Code
 from aiida.work.workchain import WorkChain, ToContext, Calc, while_
 from aiida.work.run import submit
 
-from aiida_cp2k.calculations import Cp2kCalculation
-
 from apps.scanning_probe import common
 
+from aiida_cp2k.calculations import Cp2kCalculation
 from afm import AfmCalculation
 from hrstm import HrstmCalculation
 
@@ -63,13 +62,14 @@ class HRSTMWorkChain(WorkChain):
         future = submit(Cp2kCalculation.process(), **inputs)
         return ToContext(scf_diag=Calc(future))
 
+
     def run_ppm(self):
         self.report("Running PPM")
 
         inputs = {}
         inputs['_label'] = "hrstm_ppm"
         inputs['code'] = self.inputs.ppm_code
-        inputs['parameters'] = self.inputs.ppm_parameters
+        inputs['parameters'] = self.inputs.ppm_params
         inputs['parent_calc_folder'] = self.ctx.scf_diag.out.remote_folder
         # TODO set atom types properly
         inputs['atomtypes'] = SinglefileData(file="/project/apps/scanning_probe/hrstm/atomtypes_2pp.ini")
@@ -83,46 +83,201 @@ class HRSTMWorkChain(WorkChain):
         future = submit(AfmCalculation.process(), **inputs)
         return ToContext(ppm=future)
 
+
     def run_hrstm(self):
         self.report("Running HR-STM")
 
+        # TODO this should cut the energies to the necessary heights and then store in aiida
         inputs = {}
-        # TODO
+        inputs['_label'] = "hrstm"
+        inputs['code'] = self.inputs.hrstm_code
+        inputs['parameters'] = self.inputs.hrstm_params
+        inputs['parent_calc_folder'] = self.ctx.scf_diag.out.remote_folder
+        inputs['_options'] = {
+            "resources": {"num_machines": 6},
+            "max_wallclock_seconds": 43200, # 12:00 hours
+        }
+        # Need to make an explicit instance for the node to be stored to aiida
+        settings = ParameterData(dict={'additional_retrieve_list': ['hrstm.npz']})
+        inputs['settings'] = settings
+
+        self.report("HR-STM Inputs: " + str(inputs))
 
         future = submit(HrstmCalculation.process(), **inputs)
         return ToContext(hrstm=future)
 
 
+    # ==========================================================================
+    @classmethod
+    def build_cp2k_inputs(cls, structure, cell, code,
+                          mgrid_cutoff, wfn_file_path, elpa_switch, emax):
+
+        inputs = {}
+        inputs['_label'] = "scf_diag"
+        inputs['code'] = code
+        inputs['file'] = {}
 
 
+        atoms = structure.get_ase()  # slow
+        n_atoms = len(atoms)
 
+        # structure
+        tmpdir = tempfile.mkdtemp()
+        geom_fn = tmpdir + '/geom.xyz'
+        atoms.write(geom_fn)
+        geom_f = SinglefileData(file=geom_fn)
+        shutil.rmtree(tmpdir)
 
+        inputs['file']['geom_coords'] = geom_f
 
+        cell_array = cell.get_array('cell')
 
+        # parameters
+        cell_abc = "%f  %f  %f" % (cell_array[0],
+                                   cell_array[1],
+                                   cell_array[2])
+        num_machines = 12
+        if n_atoms > 500:
+            num_machines = 27
+        walltime = 72000
 
+        wfn_file = ""
+        if wfn_file_path != "":
+            wfn_file = os.path.basename(wfn_file_path.value)
 
+        added_mos = np.max([100, int(n_atoms*emax/6.0)])
 
+        inp = cls.get_cp2k_input(cell_abc,
+                                 mgrid_cutoff,
+                                 walltime*0.97,
+                                 wfn_file,
+                                 elpa_switch,
+                                 added_mos,
+                                 atoms)
 
+        inputs['parameters'] = ParameterData(dict=inp)
 
+        # settings
+        #settings = ParameterData(dict={'additional_retrieve_list': ['aiida-RESTART.wfn', 'BASIS_MOLOPT', 'aiida.inp']})
+        #inputs['settings'] = settings
 
+        # resources
+        inputs['_options'] = {
+            "resources": {"num_machines": num_machines},
+            "max_wallclock_seconds": walltime,
+            "append_text": ur"cp $CP2K_DATA_DIR/BASIS_MOLOPT .",
+        }
+        if wfn_file_path != "":
+            inputs['_options']["prepend_text"] = ur"cp %s ." % wfn_file_path
 
+        return inputs
 
+    # ==========================================================================
+    @classmethod
+    def get_cp2k_input(cls, cell_abc, mgrid_cutoff, walltime, wfn_file, elpa_switch, added_mos, atoms):
 
+        inp = {
+            'GLOBAL': {
+                'RUN_TYPE': 'ENERGY',
+                'WALLTIME': '%d' % walltime,
+                'PRINT_LEVEL': 'LOW',
+                'EXTENDED_FFT_LENGTHS': ''
+            },
+            'FORCE_EVAL': cls.get_force_eval_qs_dft(cell_abc,
+                                                    mgrid_cutoff, wfn_file, added_mos, atoms),
+        }
 
+        if elpa_switch:
+            inp['GLOBAL']['PREFERRED_DIAG_LIBRARY'] = 'ELPA'
+            inp['GLOBAL']['ELPA_KERNEL'] = 'AUTO'
+            inp['GLOBAL']['DBCSR'] = {'USE_MPI_ALLOCATOR': '.FALSE.'}
 
+        return inp
 
+    # ==========================================================================
+    @classmethod
+    def get_force_eval_qs_dft(cls, cell_abc, mgrid_cutoff, wfn_file, added_mos, atoms):
+        force_eval = {
+            'METHOD': 'Quickstep',
+            'DFT': {
+                'BASIS_SET_FILE_NAME': 'BASIS_MOLOPT',
+                'POTENTIAL_FILE_NAME': 'POTENTIAL',
+                'QS': {
+                    'METHOD': 'GPW',
+                    'EXTRAPOLATION': 'ASPC',
+                    'EXTRAPOLATION_ORDER': '3',
+                    'EPS_DEFAULT': '1.0E-14',
+                },
+                'MGRID': {
+                    'CUTOFF': '%d' % (mgrid_cutoff),
+                    'NGRIDS': '5',
+                },
+                'SCF': {
+                    'MAX_SCF': '1000',
+                    'SCF_GUESS': 'ATOMIC',
+                    'EPS_SCF': '1.0E-7',
+                    'ADDED_MOS': str(added_mos),
+                    'CHOLESKY': 'INVERSE',
+                    'DIAGONALIZATION': {
+                        '_': '',
+                    },
+                    'SMEAR': {
+                        'METHOD': 'FERMI_DIRAC',
+                        'ELECTRONIC_TEMPERATURE': '300',
+                    },
+                    'MIXING': {
+                        'METHOD': 'BROYDEN_MIXING',
+                        'ALPHA': '0.1',
+                        'BETA': '1.5',
+                        'NBROYDEN': '8',
+                    },
+                    'OUTER_SCF': {
+                        'MAX_SCF': '15',
+                        'EPS_SCF': '1.0E-7',
+                    },
+                    'PRINT': {
+                        'RESTART': {
+                            'EACH': {
+                                'QS_SCF': '0',
+                                'GEO_OPT': '1',
+                            },
+                            'ADD_LAST': 'NUMERIC',
+                            'FILENAME': 'RESTART'
+                        },
+                        'RESTART_HISTORY': {'_': 'OFF'}
+                    }
+                },
+                'XC': {
+                    'XC_FUNCTIONAL': {'_': 'PBE'},
+                },
+                'PRINT': {
+                    'V_HARTREE_CUBE': {
+                        'FILENAME': 'HART',
+                        'STRIDE': '2 2 2',
+                    },
+                },
+            },
+            'SUBSYS': {
+                'CELL': {'ABC': cell_abc},
+                'TOPOLOGY': {
+                    'COORD_FILE_NAME': 'geom.xyz',
+                    'COORDINATE': 'xyz',
+                    'CENTER_COORDINATES': {'_': ''},
+                },
+                'KIND': [],
+            }
+        }
 
+        if wfn_file != "":
+            force_eval['DFT']['RESTART_FILE_NAME'] = "./%s"%wfn_file
+            force_eval['DFT']['SCF']['SCF_GUESS'] = 'RESTART'
 
+        used_kinds = np.unique(atoms.get_chemical_symbols())
+        for symbol in used_kinds:
+            force_eval['SUBSYS']['KIND'].append({
+                '_': symbol,
+                'BASIS_SET': common.ATOMIC_KIND_INFO[symbol]['basis'],
+                'POTENTIAL': common.ATOMIC_KIND_INFO[symbol]['pseudo'],
+            })
 
-
-
-
-
-
-
-
-
-
-
-
-
+        return force_eval
