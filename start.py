9ac78ac967f0e73ccb8803a92fef2739a4780fa6
Fabian <hifabian@github.com>
2019-07-31 09:25:54 +0000

    Rearranged simulation in start

--- start.py
+++ start.py
@@ -6,12 +6,14 @@ def get_start_widget(appbase, jupbase):
     <table>
     <tr>
         <th style="text-align:center">General</th>
-        <th style="width:60px" rowspan=2></th>
+        <th style="width:50px" rowspan=2></th>
         <th style="text-align:center">STM</th>
-        <th style="width:60px" rowspan=2></th>
+        <th style="width:50px" rowspan=2></th>
         <th style="text-align:center">ORB</th>
-        <th style="width:60px" rowspan=2></th>
+        <th style="width:50px" rowspan=2></th>
         <th style="text-align:center">PDOS</th>
+        <th style="width:50px" rowspan=2></th>
+        <th style="text-align:center">AFM</th>
     </tr>
     
     <tr>
@@ -34,22 +36,20 @@ def get_start_widget(appbase, jupbase):
             <li><a href="{appbase}/pdos/submit_pdos.ipynb" target="_blank">Submit PDOS</a>
             <li><a href="{appbase}/pdos/view_pdos.ipynb" target="_blank">View PDOS</a>
         </ul></td>
+
+        <td valign="top"><ul>
+            <li><a href="{appbase}/afm/submit_afm.ipynb" target="_blank">Submit AFM</a>
+            <li><a href="{appbase}/afm/view_afm.ipynb" target="_blank">View AFM</a>
+        </ul></td>
     </tr>
     </table>
 
     <table>
     <tr>
-        <th style="text-align:center">AFM</th>
-        <th style="width:60px" rowspan=2></th>
         <th style="text-align:center">HR-STM</th>
     </tr>
 
     <tr>
-        <td valign="top"><ul>
-            <li><a href="{appbase}/afm/submit_afm.ipynb" target="_blank">Submit AFM</a>
-            <li><a href="{appbase}/afm/view_afm.ipynb" target="_blank">View AFM</a>
-        </ul></td>
-
         <td valign="top"><ul>
             <li><a href="{appbase}/hrstm/submit_hrstm.ipynb" target="_blank">Submit HR-STM</a>
             <li><a href="{appbase}/hrstm/view_hrstm.ipynb" target="_blank">View HR-STM</a>
