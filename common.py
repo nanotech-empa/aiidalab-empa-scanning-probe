0b99ad0099bb5b48ae8140ae9c1b11d3f2f17531
Carlo Pignedoli <c.pignedoli@gmail.com>
2023-02-07 15:13:04 +0000

    unified STM AFM ORBITALS HRSTM

--- common.py
+++ common.py
@@ -464,3 +464,214 @@ def check_if_calc_ok(self_, prev_calc):
             return False
 
     return True
+
+def create_stm_parameters(extrap_plane,const_height_text,
+                          struct_symbols,
+                          parent_dir,
+                          elim_float_slider0,
+                          elim_float_slider1,
+                          de_floattext,
+                          energy_range_str,
+                          const_current_text,
+                          fwhms_text,
+                          ptip_floattext
+                          ):
+        
+        max_height = max([float(h) for h in const_height_text])
+        extrap_extent = max([max_height - extrap_plane, 5.0])
+        
+        # Evaluation region in z
+        z_min = 'n-2.0_C' if 'C' in struct_symbols else 'p-4.0'
+        z_max = 'p{:.1f}'.format(extrap_plane)
+        
+        
+        
+        energy_range_str = "%.2f %.2f %.3f" % (
+            elim_float_slider0, elim_float_slider1, de_floattext
+        )
+
+        paramdata = {
+            '--cp2k_input_file':    parent_dir+'aiida.inp',
+            '--basis_set_file':     parent_dir+'BASIS_MOLOPT',
+            '--xyz_file':           parent_dir+'aiida.coords.xyz',
+            '--wfn_file':           parent_dir+'aiida-RESTART.wfn',
+            '--hartree_file':       parent_dir+'aiida-HART-v_hartree-1_0.cube',
+            '--output_file':        'stm.npz',
+            '--eval_region':        ['G', 'G', 'G', 'G', z_min, z_max],
+            '--dx':                 '0.15',
+            '--eval_cutoff':        '16.0',
+            '--extrap_extent':      str(extrap_extent),
+            '--energy_range':       energy_range_str,
+            '--heights':            const_height_text,
+            '--isovalues':          const_current_text,
+            '--fwhms':              fwhms_text,
+            '--p_tip_ratios':       ptip_floattext,
+        }
+        return paramdata
+
+def create_orbirals_parameters(extrap_plane,
+                               heights_text,
+                               parent_dir,
+                               n_homo_inttext,
+                               n_lumo_inttext,
+                               isovals_text,
+                               fwhms_text,
+                               ptip_floattext):
+        max_height = max([float(h) for h in heights_text])
+        extrap_extent = max([max_height - extrap_plane, 5.0])
+        paramdata = {
+            '--cp2k_input_file':    parent_dir+'aiida.inp',
+            '--basis_set_file':     parent_dir+'BASIS_MOLOPT',
+            '--xyz_file':           parent_dir+'aiida.coords.xyz',
+            '--wfn_file':           parent_dir+'aiida-RESTART.wfn',
+            '--hartree_file':       parent_dir+'aiida-HART-v_hartree-1_0.cube',
+            '--orb_output_file':    'orb.npz',
+            '--eval_region':        ['G', 'G', 'G', 'G', 'n-1.0_C', 'p%.1f'%extrap_plane],
+            '--dx':                 '0.15',
+            '--eval_cutoff':        '16.0',
+            '--extrap_extent':      str(extrap_extent),
+            '--n_homo':             str(n_homo_inttext+2),
+            '--n_lumo':             str(n_lumo_inttext+2),
+            '--orb_heights':        heights_text,
+            '--orb_isovalues':      isovals_text,
+            '--orb_fwhms':          fwhms_text,
+            '--p_tip_ratios':       ptip_floattext.value,
+        }
+        return paramdata    
+
+def create_pp_parameterdata(ase_geom,
+                            dx,
+                            scanminz_floattxt,
+                            scanmaxz_floattxt,
+                            amp_floattxt,
+                            f0_cantilever_floattxt):
+    cell = ase_geom.cell
+    top_z = np.max(ase_geom.positions[:, 2])
+    paramdata = {
+        'probeType':    'Otip',
+        'charge':       -0.028108681223969645,
+        'sigma':        0.7,
+        'tip':          's',
+        'klat':         0.34901278868090491,
+        'krad':         21.913190531846034,
+        'r0Probe':      [0.0, 0.0, 2.97],
+        'PBC':          'False',
+        'gridA':        list(cell[0]),
+        'gridB':        list(cell[1]),
+        'gridC':        list(cell[2]),
+        'scanMin':      [0.0, 0.0, np.round(top_z, 1)+scanminz_floattxt],
+        'scanMax':      [cell[0,0], cell[1,1], np.round(top_z, 1)+scanmaxz_floattxt],
+        'scanStep':     [dx, dx, dx],
+        'Amplitude':    amp_floattxt,
+        'f0Cantilever': f0_cantilever_floattxt
+    }
+    return paramdata
+
+def create_2pp_parameterdata(ase_geom,
+                             dx,
+                             resp,
+                             scanminz_floattxt,
+                             scanmaxz_floattxt,
+                             amp_floattxt,
+                             f0_cantilever_floattxt):
+    cell = ase_geom.cell
+    top_z = np.max(ase_geom.positions[:, 2])
+    paramdata = {
+        'Catom':        'Ctip',
+        'Oatom':        'Otip',
+        'ChargeCuUp':   resp[0],
+        'ChargeCuDown': resp[1],
+        'Ccharge':      resp[2],
+        'Ocharge':      resp[3],
+        'sigma':        0.7,
+        'Cklat':        0.24600212465950813,
+        'Oklat':        0.15085476515590224,
+        'Ckrad':        20,
+        'Okrad':        20,
+        'rC0':          [0.0, 0.0, 1.82806112489999961213],
+        'rO0':          [0.0, 0.0, 1.14881347770000097341],
+        'PBC':          'False',
+        'gridA':        list(cell[0]),
+        'gridB':        list(cell[1]),
+        'gridC':        list(cell[2]),
+        'scanMin':      [0.0, 0.0, np.round(top_z, 1)+scanminz_floattxt],
+        'scanMax':      [cell[0,0], cell[1,1], np.round(top_z, 1)+scanmaxz_floattxt],
+        'scanStep':     [dx, dx, dx],
+        'Amplitude':    amp_floattxt,
+        'f0Cantilever': f0_cantilever_floattxt,
+        'tip':          'None',
+        'Omultipole':   's',
+    }
+    return paramdata
+
+def create_hrstm_parameterdata(parent_dir,
+                               ppm_dir,
+                               ase_geom,
+                               ppm_params_dict,
+                               tiptype_ipw,
+                               stip_ipw,
+                                pytip_ipw,
+                                pztip_ipw,
+                                pxtip_ipw,
+                                volmin_ipw,
+                                volmax_ipw,
+                                volstep_ipw,
+                                volstep_ipwmin,
+                                fwhm_ipw,
+                                wfnstep_ipw,
+                                extrap_ipw,
+                                workfun_ipw,
+                                orbstip_ipw,
+                                fwhmtip_ipw,
+                               ):
+# External folders 
+        cell = ArrayData()
+        cell.set_array('cell', np.diag(ase_geom.cell))
+                
+        # PPM folder of position
+        #ppmQK = ppm_dir+"Q%1.2fK%1.2f/" %(ppm_params_dict['Ocharge'], ppm_params_dict['Oklat'])
+        # new convention:
+        ppmQK = ppm_dir+"Qo%1.2fQc%1.2fK%1.2f/" % (ppm_params_dict['Ocharge'], ppm_params_dict['Ccharge'],
+                                                   ppm_params_dict['Oklat'])
+        
+        # Tip type to determine PDOS and PPM position files
+        if tiptype_ipw != "parametrized":
+            pdos_list = tiptype_ipw
+            path = os.path.dirname(hrstm_code.get_remote_exec_path())+"/hrstm_tips/"
+            pdos_list = [path+"tip_coeffs.tar.gz"]
+            tip_pos = [ppmQK+"PPpos", ppmQK+"PPdisp"]
+        else: # Parametrized tip
+            pdos_list = [str(stip_ipw), str(pytip_ipw), 
+                         str(pztip_ipw), str(pxtip_ipw)]
+            tip_pos = ppmQK+"PPdisp"
+
+        # HRSTM parameters
+        paramdata = {
+            '--output':          'hrstm',
+            '--voltages':        [str(val) for val in np.round(np.arange(
+                volmin_ipw,
+                volmax_ipw+volstep_ipw, 
+                volstep_ipw), 
+                len(str(volstep_ipwmin).split('.')[-1])).tolist()],
+            # Sample information
+            '--cp2k_input_file': parent_dir+'aiida.inp',
+            '--basis_set_file':  parent_dir+'BASIS_MOLOPT',
+            '--xyz_file':        parent_dir+'geom.xyz',
+            '--wfn_file':        parent_dir+'aiida-RESTART.wfn',
+            '--hartree_file':    parent_dir+'aiida-HART-v_hartree-1_0.cube',
+            '--emin':            str(volmin_ipw-2.0*fwhm_ipw),
+            '--emax':            str(volmax_ipw+2.0*fwhm_ipw),
+            '--fwhm_sam':        str(fwhm_ipw),
+            '--dx_wfn':          str(wfnstep_ipw),
+            '--extrap_dist':     str(extrap_ipw),
+            '--wn':              str(workfun_ipw),
+            # Tip information
+            '--pdos_list':       pdos_list,
+            '--orbs_tip':        str(orbstip_ipw),
+            '--tip_shift':       str(ppm_params_dict["rC0"][2]+ppm_params_dict["rO0"][2]),
+            '--tip_pos_files':   tip_pos,
+            '--fwhm_tip':        str(fwhmtip_ipw),
+        }
+        if rotate_ipw.value:
+            paramdata['--rotate'] = ''   
+        return paramdata 
